efbb3546fe5fc9fba19088f09c1a9e7ae0540e44
eugenebbr <eugene.bbr@gmail.com>
2020-07-29 14:28:59 +0300

    Main tasks completed

--- kubernetes/terraform/2st_version/main.tf
+++ kubernetes/terraform/2st_version/main.tf
@@ -115,6 +115,63 @@ resource "google_container_node_pool" "node_pool" {
   }
 }
 
+resource "google_container_node_pool" "big_node_pool" {
+  provider = google-beta
+
+  name     = "bigpool"
+  project  = var.project
+  location = var.location
+  cluster  = module.gke_cluster.name
+
+  initial_node_count = "1"
+
+  # autoscaling {
+  #   min_node_count = "1"
+  #   max_node_count = "5"
+  # }
+
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "true"
+  }
+
+  node_config {
+    image_type   = "COS"
+    machine_type = "n1-standard-2"
+
+    labels = {
+      pool = "gitlab"
+    }
+
+    # Add a public tag to the instances. See the network access tier table for full details:
+    # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+    tags = [
+      module.vpc_network.public,
+      "public-pool-example",
+    ]
+
+    disk_size_gb = "30"
+    disk_type    = "pd-standard"
+    preemptible  = false
+
+    service_account = module.gke_service_account.email
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE A CUSTOM SERVICE ACCOUNT TO USE WITH THE GKE CLUSTER
 # ---------------------------------------------------------------------------------------------------------------------
